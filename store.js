d0f61aff7ab9812ace9229f86096d416017a3769
Steeve Payraudeau <16964532+steevepay@users.noreply.github.com>
2024-04-25 13:47:43 +0400

    minimum_example_to_reproduce

--- store.js
+++
@@ -1,7 +0,0 @@
-import { RiotMeiosis } from '@riot-tools/meiosis';
-
-const state = {
-    loading: false
-}
-
-export default new RiotMeiosis(state, { flushOnRead: false, statesToKeep: 1 });
--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import riot from 'rollup-plugin-riot'
 
 export default defineConfig({
-//   root       : process.cwd() + '/client',
   plugins    : [riot()],
   build: { 
     minify       : 'esbuild', /** https://vitejs.dev/config/build-options.html#build-minify */
